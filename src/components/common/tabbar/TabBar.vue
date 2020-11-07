6555a07229759242cfc8b000c42e79adad5a324c
wang_liang <3469246179@qq.com>
2020-11-07 09:20:41 +0800

    “第一次提交”

---
+++ src/components/common/tabbar/TabBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="tab-bar">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: ""
+  }
+</script>
+
+<style scoped>
+  #tab-bar{
+    position: fixed;
+    height: 55px;
+    bottom: 0px;
+    left: 0px;
+    right: 0px;
+    display: flex;
+    text-align: center;
+    background: #ccc;
+  }
+
+</style>
---
+++ src/components/content/MainTab.vue
@@ -0,0 +1,38 @@
+<template>
+  <TabBar>
+    <TabBarItem path="/home" col="green">
+      <div slot="icon" class="fa fa-home item-icon" ></div>
+      <div slot="text">首页</div>
+    </TabBarItem>
+    <TabBarItem path="/category" col="green">
+      <div slot="icon" class="fa fa-th-large item-icon" ></div>
+      <div slot="text">分类</div>
+    </TabBarItem>
+    <TabBarItem path="/shoppingCart" col="green">
+      <div slot="icon" class="fa fa-shopping-cart item-icon" ></div>
+      <div slot="text">购物车</div>
+    </TabBarItem>
+    <TabBarItem path="/profile" col="green">
+      <div slot="icon" class="fa fa-user item-icon" ></div>
+      <div slot="text">我的</div>
+    </TabBarItem>
+  </TabBar>
+</template>
+
+<script>
+  import TabBar from "../common/tabbar/TabBar"
+  import TabBarItem from "../common/tabbar/TabBarItem"
+  export default {
+    name: "",
+    components : {
+      TabBar,
+      TabBarItem
+    }
+  }
+</script>
+
+<style scoped>
+  .tab-bar-item .item-icon{
+    font-size: 26px;
+  }
+</style>
